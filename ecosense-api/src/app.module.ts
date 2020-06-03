4f0ea377acfcf6e96c560603d02558e2499cbeff
Luis Fernando <luispoma.frag@gmail.com>
2020-06-03 20:26:57 -0300

    create database conection and create collect module

--- ecosense-api/src/app.module.ts
+++ ecosense-api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { CollectModule } from './collect/collect.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), CollectModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ ecosense-api/src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3308,
+  username: 'luis',
+  password: 'coxinha',
+  database: 'ecosense_db',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: false,
+};
