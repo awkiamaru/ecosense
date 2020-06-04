0725b2887583439ecf2304a9b370bb302d94ca70
Luis Fernando <luispoma.frag@gmail.com>
2020-06-04 00:53:16 -0300

    create find by id endpoint

--- ecosense-api/src/points/points.controller.ts
+++ ecosense-api/src/points/points.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { PointsService } from './points.service';
 import { CreatePointDTO } from './dto/point.dto';
+import { Point } from './points.entity';
 
 @Controller('points')
 export class PointsController {
   constructor(private pointService: PointsService) {}
+
+  @Get('/:id')
+  public getPointById(@Param('id') id: number): Promise<Point> {
+    return this.pointService.findById(id);
+  }
 
   @Post()
   public createPoint(@Body() pointDTO: CreatePointDTO) {
--- ecosense-api/src/points/points.service.ts
+++ ecosense-api/src/points/points.service.ts
@@ -1,12 +1,19 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PointRepository } from './points.repository';
 import { CreatePointDTO } from './dto/point.dto';
 import { PointItemRepository } from './point-item.repository';
 import { PointItem } from './point-item.entity';
+import { Point } from './points.entity';
 
 @Injectable()
 export class PointsService {
+  private logger = new Logger('PointService');
   constructor(
     @InjectRepository(PointRepository)
     private pointRepository: PointRepository,
@@ -14,9 +21,18 @@ export class PointsService {
     private pointItemRepository: PointItemRepository,
   ) {}
 
+  public async findById(pointId: number): Promise<Point> {
+    const foundPoint = await this.pointRepository.findOne({
+      where: { pointId },
+    });
+    if (!foundPoint) {
+      throw new NotFoundException(`Point id ${pointId} not found`);
+    }
+    return foundPoint;
+  }
+
   public async createNewPoint(pointDTO: CreatePointDTO) {
     const point = await this.pointRepository.savePoint(pointDTO);
-    Logger.log(JSON.stringify(point));
     const pointItems = Array<PointItem>();
     pointDTO.items.map((id: number) => {
       const pointItem = new PointItem();
@@ -25,10 +41,10 @@ export class PointsService {
       pointItems.push(pointItem);
     });
     try {
-      Logger.log(JSON.stringify(pointItems));
       await this.pointItemRepository.save(pointItems);
     } catch (error) {
-      Logger.error('ERRO', error.stack);
+      this.logger.error('Failed insert itemsPoint', error.stack);
+      throw new InternalServerErrorException('Failed save Point with item');
     }
   }
 }
